e35753fd4edac59134f7011a9ca88a81e2d77a46
leanny01 <dleannykalangu@gmail.com>
2020-09-19 09:47:55 +0200

    added express file

---
+++ server/express.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const path  = require( 'path')
+const bodyParser= require('body-parser')
+const cookieParser = require('cookie-parser')
+const compress = require('compression')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const CURRENT_WORKING_DIR = process.cwd()
+const app = express()
+
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure apps by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+  }else if (err) {
+    res.status(400).json({"error" : err.name + ": " + err.message})
+    console.log(err)
+  }
+})
+
+export default app
